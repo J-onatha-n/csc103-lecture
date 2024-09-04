5ce232d7500ebae5cec5eec06a26f19f6d01bce7
Jonathan <jsanche046@citymail.ccny.cuny.edu>
2024-09-04 18:33:52 -0400

    Updated program that produces second smallest number inputted.

--- 01/notes_largest.cpp
+++ 01/notes_largest.cpp
@@ -3,61 +3,72 @@ using std::cin;
 using std::cout;
 #include <limits.h> //needed for INT_MIN
 int main() {
-    //test for smallest
-  int smallest = INT_MAX;
-  int input;
-  int secondSmallest = INT_MAX;
+	//test for smallest
+	int smallest = INT_MAX;
+	int input;
+	int secondSmallest = INT_MAX;
 
 
-  while(cin >> input){
-    if (input < smallest && smallest <= secondSmallest) {
-      cout << "new smallest\n";
-      secondSmallest = smallest;
-      smallest = input;
+	while(cin >> input){
+		if (input < smallest) { //&& smallest <= secondSmallest) {
 
+			secondSmallest = smallest;
+			smallest = input;
+			cout << "Smallest number is " << smallest << "\n";
+			cout << "Second smallest number is " << secondSmallest<< "\n";
+		}
 
-      }
+		if (input < secondSmallest && smallest < input) {
+			secondSmallest = input;
+			cout << "Second smallest number is " << secondSmallest<< "\n";
+			}
 
-    if (secondSmallest <= input && smallest < secondSmallest) {
-      cout << "new second smallest\n";
-      secondSmallest = input;
-      }
+		}
 
+		cout << "Second smallest number was " << secondSmallest <<"\n";
+		cout << "Smallest number was " << smallest <<"\n";
+		return 0;
 
+		/* if (secondSmallest <= input && smallest < secondSmallest) {
 
-   /* if(input < smallest < secondSmallest) {
-      smallest = input;
-      } else if (smallest < input < secondSmallest) {
-      secondSmallest = input;
-      }
-*/
-      cout << "Second smallest number was " << secondSmallest <<"\n";
-      cout << "Smallest number was " << smallest <<"\n";
-    }
-
-
-
-  //cout << "Input numbers to determine the largest! ";
-
-
-  /*
-  int largest = INT_MIN;
-  int input;
-  cin >> input;
-
-
-  while (cin >> input) {
-      //cout << "Enter a number: ";
-    if(input > largest) {
-      largest = input;
-    }
-  }
-*/
+		//secondSmallest = smallest;
+		}
+		*/
 
 
 
+		//first iteration did not work
 
-  //cout << "Largest number was " << largest <<"\n";
+		/* if(input < smallest < secondSmallest) {
+		   smallest = input;
+		   } else if (smallest < input < secondSmallest) {
+		   secondSmallest = input;
+		   }
+		   */
 
-  return 0;
-}
+	}
+
+
+
+	//cout << "Input numbers to determine the largest! ";
+
+
+	/*
+	   int largest = INT_MIN;
+	   int input;
+	   cin >> input;
+
+
+	   while (cin >> input) {
+	//cout << "Enter a number: ";
+	if(input > largest) {
+	largest = input;
+	//~ }
+	}
+	*/
+
+
+
+
+	//cout << "Largest number was " << largest <<"\n";
+
